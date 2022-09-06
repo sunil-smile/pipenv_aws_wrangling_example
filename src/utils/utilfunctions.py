6c0e91882644fe4e0ff6e4068a8944452e233092
sunil-ygroup <sunilprasath.elangovan@ygroupcompanies.com>
2022-09-06 12:46:58 +0200

    added dq using great expectations

--- src/utils/utilfunctions.py
+++ src/utils/utilfunctions.py
@@ -5,6 +5,7 @@ from typing import List, Optional
 
 import awswrangler as wr
 import boto3
+import numpy as np
 import pandas as pd
 import pyspark.sql.functions as F
 from pyspark.sql import SparkSession
@@ -199,4 +200,77 @@ class UtilFunctions:
         for column in columns_to_transform:
             if column in df.columns:
                 df[column] = df[column].str.upper()
+        return df
+
+    @classmethod
+    def __convert_categorical(cls, df: pd.DataFrame, col: str) -> pd.DataFrame:
+        """This function converts the Python dataframe object datatype to categorical if the average of unique values is less than 0.33
+
+        Arguments:
+                df : pd.DataFrame
+                    description = Input pandas dataframe
+                col : str
+                    description = Object data type column name
+
+        Returns:
+                pd.DataFrame         =  returns the dataframe which changes object datatype to categorical if the conditions are met
+        """
+        assert isinstance(df, pd.DataFrame), "{df} is not a Pandas DataFrame"
+        num_unique_values = df[col].nunique()
+        num_total_values = len(df[col])
+        if (num_total_values != 0) and (num_unique_values / num_total_values < 0.33):
+            df.loc[:, col] = df[col].astype("category")
+        return df
+
+    # https://www.mikulskibartosz.name/how-to-reduce-memory-usage-in-pandas/
+    @classmethod
+    def reduce_mem_usage(cls, df: pd.DataFrame) -> pd.DataFrame:
+        """This function optimize the pandas dataframe datatypes based on the min and max value range of the column assign the correct
+        datatype ,for instance downcasting int64 to int8 . which reduces the memory usage
+
+        Arguments:
+                df : pd.DataFrame
+                    description = Input pandas dataframe
+
+        Returns:
+                pd.DataFrame         = returns the dataframe with correct memory optimized datatype
+        """
+        start_mem = df.memory_usage().sum() / 1024**2
+        print("Memory usage of dataframe is {:.2f} MB".format(start_mem))
+        for col in df.columns:
+            col_type = df[col].dtype
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == "int":
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.uint8).min and c_max < np.iinfo(np.uint8).max:
+                    df[col] = df[col].astype(np.uint8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.uint16).min and c_max < np.iinfo(np.uint16).max:
+                    df[col] = df[col].astype(np.uint16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.uint32).min and c_max < np.iinfo(np.uint32).max:
+                    df[col] = df[col].astype(np.uint32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+                elif c_min > np.iinfo(np.uint64).min and c_max < np.iinfo(np.uint64).max:
+                    df[col] = df[col].astype(np.uint64)
+            elif str(col_type)[:5] == "float":
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            df = cls.__convert_categorical(df, col)
+
+        end_mem = df.memory_usage().sum() / 1024**2
+        print("Memory usage after optimization is: {:.2f} MB".format(end_mem))
+        print("Decreased by {:.1f}%".format(100 * (start_mem - end_mem) / start_mem))
+
         return df
